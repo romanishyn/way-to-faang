986a7329ac5c91effc2f1c2925569b7f7d1cd094
Volodymyr Romanishyn <romanishyn1@gmail.com>
2020-03-15 17:17:47 +0200

    added 15. 3Sum

--- leet_code/array/15_m_3Sum/solution.hpp
+++ leet_code/array/15_m_3Sum/solution.hpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/two-sum/
+//https://leetcode.com/problems/3sum/
 
 #pragma once
 
@@ -10,30 +10,6 @@ using std::unordered_map;
 
 class Solution {
 public:
-//    vector<int> twoSum(vector<int>& nums, int target) {
-//        const int size = nums.size();
-//        for( int i = 0; i < size; ++i ) {
-//            for( int j = i + 1; j < size; ++j ) {
-//                if( (nums[i] + nums[j]) == target )
-//                    return { i, j };
-//            }
-//        }
-//
-//        return {};
-//    }
-
-
-    vector<int> twoSum(vector<int>& nums, int target) {
-        std::unordered_map<int, int> map;
-        for( int i = 0; i < nums.size(); ++i ) {
-            auto it = map.find( target - nums[i] );
-            if( it != map.end() ) {
-                return { it->second, i };
-            } else {
-                map[ nums[ i ] ] = i;
-            }
-        }
-
-        return {};
+    vector<vector<int>> threeSum(vector<int>& nums) {
     }
 };
