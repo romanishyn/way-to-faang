cb0c8899a64c5f18bfb88bb1797d27a1af81ef87
Volodymyr Romanishyn <romanishyn1@gmail.com>
2020-07-06 12:05:35 +0300

    revised 227, 57

---
+++ leet_code/string/227_m_basic_calculator_ii/solution.cpp
@@ -0,0 +1,11 @@
+/*
+227. Basic Calculator II
+*/
+
+/*
+ * It's a Stack problem. Reverse Polish Notation
+ *
+ * Reverse Polish Notation BtB https://www.youtube.com/watch?v=qN8LPIcY6K4
+ * Infix to reverse polish using a stack: https://www.youtube.com/watch?v=LQ-iW8jm6Mk
+ * Обратная польская запись https://habr.com/ru/post/100869/
+ * */
