a0b40e54cf47cfa680a7c59d080bfe3d56b82af3
Volodymyr Romanishyn <romanishyn1@gmail.com>
2020-07-06 10:56:18 +0300

    solved 300. Longest Increasing Subsequence

--- leet_code/dynamic_programming/300_m_longest_increasing_subsequence/solution.hpp
+++ leet_code/dynamic_programming/300_m_longest_increasing_subsequence/solution.hpp
@@ -18,9 +18,10 @@ using std::vector;
  * - impl nlog(n)
  * */
 
+namespace V1 {
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    int lengthOfLIS( vector< int > & nums ) {
         if( nums.empty() )
             return 0;
 
@@ -38,6 +39,33 @@ public:
 
         return res;
     }
-
-
 };
+} // namespace V1
+
+namespace V2 {
+/*
+ * Time (N^2)
+ * Space (N)
+ * */
+class Solution {
+public:
+    int lengthOfLIS( vector< int > & nums ) {
+        if( nums.size() < 2 )
+            return nums.size();
+
+        int maxLength = 0;
+        std::vector< int > dp( nums.size(), 1 );
+
+        for( int i = 0; i < nums.size() - 1; ++i ) {
+            for( int j = i + 1; j < nums.size(); ++j ) {
+                if( nums[ i ] < nums[ j ] )
+                    dp[ j ] = std::max( dp[ j ], dp[ i ] + 1 );
+
+                maxLength = std::max( maxLength, dp[ j ] );
+            }
+        }
+
+        return maxLength;
+    }
+};
+} // namespace V2
