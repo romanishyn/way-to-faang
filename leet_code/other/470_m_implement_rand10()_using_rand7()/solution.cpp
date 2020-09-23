ae192773eb3a8f3e1421c034530a254b3bef55fd
Volodymyr Romanishyn <romanishyn1@gmail.com>
2020-09-23 13:00:41 +0300

    added new solution. 470. Implement Rand10() Using Rand7()

--- leet_code/other/470_m_implement_rand10()_using_rand7()/solution.cpp
+++ leet_code/other/470_m_implement_rand10()_using_rand7()/solution.cpp
@@ -32,6 +32,7 @@ public:
 namespace {
 /*
 https://leetcode.com/problems/implement-rand10-using-rand7/discuss/150301/Three-line-Java-solution-the-idea-can-be-generalized-to-"Implement-RandM()-Using-RandN()"/246718
+https://leetcode.com/problems/implement-rand10-using-rand7/discuss/150301/Three-line-Java-solution-the-idea-can-be-generalized-to-"Implement-RandM()-Using-RandN()"/329534
 */
 class Solution {
 public:
