bd416138f124f87a2b728f8e46a5dab1fd4ad0f7
Volodymyr Romanishyn <romanishyn1@gmail.com>
2020-09-26 13:48:29 +0300

    solved 528. Random Pick with Weight

---
+++ leet_code/other/528_m_random_pick_with_weight/solution.cpp
@@ -0,0 +1,53 @@
+/*
+https://leetcode.com/problems/random-pick-with-weight/
+*/
+
+#include <vector>
+
+using std::vector;
+
+namespace {
+/*
+N - size of w
+
+Constructor: Time O(N)
+pickIndex:   Time O(logN)
+Space O(N)
+*/
+class Solution {
+    std::vector< int > m_prefixSum;
+public:
+    Solution(vector<int>& w){
+        std::srand( std::time( nullptr ) );
+
+        std::partial_sum( w.begin(), w.end(), std::back_inserter( m_prefixSum ) );
+    }
+
+    int pickIndex() {
+        int value = ( std::rand() % m_prefixSum.back() ) + 1;
+
+        return lowerBound( m_prefixSum, value );
+    }
+
+private:
+    int lowerBound( const std::vector< int >& nums, int key ) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int ans = nums.size();
+
+        while( left <= right ) {
+            const int midIdx = left + ( right - left ) / 2;
+
+            if( nums[ midIdx ] >= key ) {
+                ans = midIdx;
+                right = midIdx - 1;
+            }
+            else  {
+                left = midIdx + 1;
+            }
+        }
+
+        return ans;
+    }
+};
+} // namespace
