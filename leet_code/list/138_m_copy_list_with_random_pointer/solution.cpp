7f5d5a77eb7c15400ca345898e805b17deb5a162
Volodymyr Romanishyn <romanishyn1@gmail.com>
2020-10-30 19:26:57 +0200

    solved 138. Copy List with Random Pointer

---
+++ leet_code/list/138_m_copy_list_with_random_pointer/solution.cpp
@@ -0,0 +1,165 @@
+/*
+https://leetcode.com/problems/copy-list-with-random-pointer/
+*/
+
+#include <unordered_map>
+
+namespace {
+/*
+N - number of nodes
+
+Time O(N)
+Space O(N)
+*/
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        std::unordered_map< Node*, Node* > originalCopy;
+        return cloneNode( head, originalCopy );
+    }
+
+private:
+    Node* cloneNode( Node* node, std::unordered_map< Node*, Node* >& originalCopy ) {
+        if( ! node )
+            return node;
+
+        if( auto it = originalCopy.find( node ); it != originalCopy.end() )
+            return it->second;
+
+        auto copyNode = new Node( node->val );
+        originalCopy.try_emplace( node, copyNode );
+
+        copyNode->next = cloneNode( node->next, originalCopy );
+        copyNode->random = cloneNode( node->random, originalCopy );
+
+        return copyNode;
+    }
+};
+} // namespace
+
+namespace {
+/*
+N - number of nodes
+
+Time O(N)
+Space O(N)
+*/
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if( ! head )
+            return head;
+
+        std::unordered_map< Node*, Node* > originalCopy;
+
+        auto curr = head;
+        while( curr ) {
+            originalCopy.try_emplace( curr, new Node( curr->val ) );
+            curr = curr->next;
+        }
+
+        curr = head;
+        while( curr ) {
+            auto clone = originalCopy[ curr ];
+            clone->next = originalCopy[ curr->next ];
+            clone->random = originalCopy[ curr->random ];
+
+            curr = curr->next;
+        }
+
+        return originalCopy[ head ];
+    }
+};
+} // namespace
+
+namespace {
+/*
+N - number of nodes
+
+Time O(N)
+Space O(1)
+*/
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if( ! head )
+            return head;
+
+        auto curr = head;
+        while( curr ) {
+            auto clone = new Node( curr->val );
+            clone->next = curr->next;
+            curr->next = clone;
+            curr = clone->next;
+        }
+
+        curr = head;
+        while( curr ) {
+            if( curr->random )
+                curr->next->random = curr->random->next;
+
+            curr = curr->next->next;
+        }
+
+        auto copyHead = head->next;
+        auto copyCurr = copyHead;
+        curr = head;
+
+        while( curr ) {
+            curr->next = copyCurr->next;
+            curr = curr->next;
+            if( curr ) {
+                copyCurr->next = curr->next;
+                copyCurr = copyCurr->next;
+            }
+        }
+
+        return copyHead;
+    }
+};
+} // namespace
