9581fc89fab4c8ba9b9d12cfd706615aeba627df
Vladimir Romanishyn <romanishyn1@gmail.com>
2021-08-03 22:51:53 +0300

    top 50 fb. 56. Merge Intervals

--- leet_code/interval/56_m_merge_intervals/solution.hpp
+++ leet_code/interval/56_m_merge_intervals/solution.hpp
@@ -76,3 +76,27 @@ public:
     }
 };
 }
+
+namespace {
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        auto comparator = [](const auto& lhs, const auto& rhs) {
+            return lhs[0] < rhs[0];
+        };
+        std::sort(begin(intervals), end(intervals), comparator);
+
+        std::vector< std::vector< int > > result;
+        result.push_back( intervals[ 0 ] );
+
+        for( int i = 1; i < intervals.size(); ++i ) {
+            if( result.back()[1] >= intervals[ i ][ 0 ] )
+                result.back()[1] = std::max( result.back()[1], intervals[ i ][ 1 ] );
+            else
+                result.push_back( intervals[ i ] );
+        }
+
+        return result;
+    }
+};
+} // namespace
