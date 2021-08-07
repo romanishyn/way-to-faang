3abc1b2d467357729898f691bb957244e4c80b06
Vladimir Romanishyn <romanishyn1@gmail.com>
2021-08-07 22:09:01 +0300

    top 50 fb. 273. Integer to English Words

--- leet_code/string/273_h_integer_to_english_words/solution.cpp
+++ leet_code/string/273_h_integer_to_english_words/solution.cpp
@@ -172,3 +172,80 @@ private:
     std::vector< std::string > ones{ "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
 };
 } // namespace
+
+namespace {
+// Terible solution
+class Solution {
+    using Tokens = std::vector< std::string >;
+    
+    std::array< std::string, 4 > trio{ "", "Thousand", "Million", "Billion" };
+
+    std::array < std::string, 20 > tens{ "", "One", "Two", "Three", "Four", "Five",
+                                   "Six", "Seven", "Eight", "Nine",
+                                   "Ten", "Eleven", "Twelve", "Thirteen",
+                                   "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+                                   "Eighteen", "Nineteen" };
+    std::array< std::string, 10 > highTens{ "","", "Twenty", "Thirty", "Forty", "Fifty",
+                                      "Sixty", "Seventy", "Eighty", "Ninety" };
+    
+    std::string concat( const std::vector< std::string >& tokens ) {
+        std::string result;
+        for( const auto& token : tokens ) {
+            if( !token.empty() )
+                result += token + " ";
+        }
+        
+        if( ! result.empty() )
+            result.pop_back();
+        
+        return result;
+    }
+    
+    std::string convertTreeDigit( int digits ) {
+        int tensNum = digits % 100;
+        digits /= 100;
+        
+        Tokens tokens;
+        if( digits ) {
+            tokens.push_back( tens[ digits ] );
+            tokens.push_back( "Hundred" );
+        }
+        
+        if( tensNum < 20 )
+            tokens.push_back( tens[ tensNum ] );
+        else {
+            tokens.push_back( highTens[ tensNum / 10 ] );
+            tokens.push_back( tens[ tensNum % 10 ] );
+        }
+        
+        return concat( tokens );
+    }
+    
+public:
+    string numberToWords(int num) {
+        std::vector< std::string > components;
+        
+        for( int i = 0; num; ++i ) {
+            int threeDigit = num % 1000;
+            num /= 1000;
+            
+            auto threeDigitStr = convertTreeDigit( threeDigit );
+            if( !trio[ i ].empty() && !threeDigitStr.empty() )
+                components.push_back( trio[ i ] );
+            
+            if( !threeDigitStr.empty() )
+                components.push_back( threeDigitStr );
+        }
+        
+        if( components.empty() )
+            return "Zero";
+        
+        std::string result;
+        for( int i = components.size() - 1; i >= 0; --i )
+            result += components[ i ] + " ";
+        result.pop_back();
+        
+        return result;
+    }
+};
+} // namespace
