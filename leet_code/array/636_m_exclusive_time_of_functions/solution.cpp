8cf5c5770afc21154ce4c85277e44e2eac01ddc3
Vladimir Romanishyn <romanishyn1@gmail.com>
2021-08-12 17:19:03 +0300

    top 50 fb. 636. Exclusive Time of Functions

--- leet_code/array/636_m_exclusive_time_of_functions/solution.cpp
+++ leet_code/array/636_m_exclusive_time_of_functions/solution.cpp
@@ -63,3 +63,59 @@ public:
     }
 };
 } // namespace
+
+namespace {
+/*
+N - number of logs
+
+Time O(N)
+Space O(N)
+*/
+class Solution {
+    struct Log {
+      int id;
+      std::string op;
+      int timestamp;
+    };
+    
+    Log getEntry( const std::string& str ) {
+        std::stringstream ss{ str };
+        std::string id;
+        std::string op;
+        std::string time;
+        const char delim = ':';
+        
+        std::getline( ss, id, delim );
+        std::getline( ss, op, delim );
+        std::getline( ss, time, delim );
+        
+        return Log{ std::stoi( id ), op, std::stoi( time ) };
+    }
+    
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        std::vector< int > result( n );
+        
+        std::stack< Log > stack;
+        for( const auto& log : logs ) {
+            Log entry = getEntry( log );
+            
+            if( entry.op == "start" ) {
+                stack.push( entry );
+            }
+            else {
+                Log start = stack.top();
+                stack.pop();
+                
+                int elapsed = entry.timestamp - start.timestamp + 1;
+                result[ start.id ] += elapsed;
+                
+                if( ! stack.empty() )
+                    result[ stack.top().id ] -= elapsed;
+            }
+        }
+        
+        return result;
+    }
+};
+} // namespace
