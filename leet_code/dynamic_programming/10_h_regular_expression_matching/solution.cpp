3407b0e9f3bea7bbe51dae908083d1f9eb6a1a92
Vladimir Romanishyn <romanishyn1@gmail.com>
2021-10-13 21:35:14 +0300

    10. Regular Expression Matching

--- leet_code/dynamic_programming/10_h_regular_expression_matching/solution.cpp
+++ leet_code/dynamic_programming/10_h_regular_expression_matching/solution.cpp
@@ -101,3 +101,53 @@ private:
 namespace V3 {
 // TODO: DP bottom-up
 } // namespace V3
+
+
+namespace {
+/*
+S - s.size
+P - p.size
+
+Time O(S*P)
+Space O(S*P)
+*/
+class Solution {
+    enum class State {
+        Unknown,
+        True,
+        False
+    };
+    using Memo = std::vector< std::vector< State > >;
+
+public:
+    bool isMatch(string s, string p) {
+        Memo memo( s.size() + 1, std::vector< State >( p.size() + 1, State::Unknown ) );
+        return isMatch( s, p, 0, 0, memo );
+    }
+
+private:
+    bool isMatch( const std::string& s, const std::string& p, int sIdx, int pIdx, Memo& memo ) {
+        if( memo[ sIdx ][ pIdx ] != State::Unknown )
+            return memo[ sIdx ][ pIdx ] == State::True;
+
+        bool result = false;
+
+        if( pIdx == p.size() ) {
+            result = sIdx == s.size();
+        }
+        else {
+            const bool firstMatch = sIdx >= s.size() ? false : s[ sIdx ] == p[ pIdx ] || p[ pIdx ] == '.';
+
+            if( p.size() - pIdx >= 2 && p[ pIdx + 1 ] == '*' ) {
+                result = isMatch( s, p, sIdx, pIdx + 2, memo ) 
+                    ||  ( firstMatch && isMatch( s, p, sIdx + 1, pIdx, memo ) );
+            }
+            else
+                result = firstMatch && isMatch( s, p, sIdx + 1, pIdx + 1, memo );
+        }
+
+        memo[ sIdx ][ pIdx ] = result ? State::True : State::False;
+        return result;
+    }
+};
+} // namespace
